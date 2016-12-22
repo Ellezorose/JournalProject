dfff08862bd3eceb56133e8d81b1fd309d0aeb26
Ellezorose <r.lucenio1995@gmail.com>
2016-12-21 23:54:47 -0800

    it works

---
+++ include/header.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Journal <?php echo $title; ?> </title>
+		<link rel="stylesheet" href="css/bootstrap.css">
+		<link rel="stylesheet" href="css/homepage.css">
+		<script src="js/bootstrap.js"></script>
+	</head>
+	<body>
+		
+	</body>
+</html>
--- index.php
+++ index.php
@@ -1,41 +1,29 @@
-<!doctype html>
-<html>
-	<head>
-		<meta charset="utf-8">
-		<title>Journal</title>
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/homepage.css">
-		<script src="js/bootstrap.js"></script>
-	</head>
-	<body>
-	 	
-		<div id="nav-container">
-			<div id="wrapper">
-					
-					
-						<div class="width-1-1">
-							<a href="journalentry.php"> NEW ENTRY  </a></li>
-						</div>
-						
-							<!-- <li><a href="life.html"> LIFE </a></li> -->
-						
-						<div class="width-1-2">
-							<a href="calendar.php"> CALENDAR </a></li>
-						</div>
-
-						<div class="width-1-2">
-							<a href="tags.php"> TAGS </a></li>
-						</div>
-
-						<div class="width-1-1">
-							<a href="settings.php"> SETTINGS </a></li>
-						</div>
-
-							<!-- <li><a href="#"> CONTACT </a></li> -->
-					
+<?php include_once($_SERVER["DOCUMENT_ROOT"] . '/include/header.php'); ?>
+<div id="nav-container">
+	<div id="wrapper">
+			
+			
+				<div class="width-1-1">
+					<a href="journalentry.php"> NEW ENTRY  </a></li>
+				</div>
 				
-			</div>
-		</div>
-	</body>
+					<!-- <li><a href="life.html"> LIFE </a></li> -->
+				
+				<div class="width-1-2">
+					<a href="calendar.php"> CALENDAR </a></li>
+				</div>
 
-</html>
+				<div class="width-1-2">
+					<a href="tags.php"> TAGS </a></li>
+				</div>
+
+				<div class="width-1-1">
+					<a href="settings.php"> SETTINGS </a></li>
+				</div>
+
+					<!-- <li><a href="#"> CONTACT </a></li> -->
+			
+		
+	</div>
+</div>
+<?php include_once($_SERVER["DOCUMENT_ROOT"] . '/include/footer.php'); ?>
